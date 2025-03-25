f296e58b9c3d162da37805762f3b42fccc180f1d
chiragml <malhotra.chdelhi@gmail.com>
2025-03-25 02:38:40 +0000

    updated preprocessing code and added the data download code

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,5 @@
+#!/user/bin/env python
+#!pip install pybase64
 '''
 -------------------------------
 Description
@@ -21,70 +23,90 @@ import cv2
 import glob
 from PIL import Image
 import pybase64
+import argparse
 
-spark = SparkSession.builder.appName("preprocessing-data").config("spark.memory.offHeap.enabled","true").config("spark.memory.offHeap.size","10g").getOrCreate()
+def main(csv_file_path, video_path, save_path):
+    spark = SparkSession.builder.appName("preprocessing-data").config("spark.memory.offHeap.enabled","true").config("spark.memory.offHeap.size","10g").getOrCreate()
 
-# Loading the csv
-df_csv = spark.read.csv(os.getcwd() + "/data/OpenVid-1M.csv", header=True)
+    # Loading the csv
+    df_csv = spark.read.csv(csv_file_path, header=True)
 
-video_dir = "/data/video/video/"
-video_path = os.getcwd() + video_dir
-save_path = "./data/"
-# Getting the video names
-videos = glob.glob(video_path + '*.mp4')
-# videos = pathlib.Path(video_path).glob('*.mp4 ')
-video_list = [str(vids) for vids in videos]
+    video_dir = "/data/video/video/"
+    video_path = video_path
+    # Getting the video names
+    videos = glob.glob(video_path + '*.mp4')
+    # videos = pathlib.Path(video_path).glob('*.mp4 ')
+    video_list = [str(vids) for vids in videos]
 
-# Getting the video names
-video_names = [vid.split('/')[-1] for vid in video_list]
-# print(video_list[0:10])
+    # Getting the video names
+    video_names = [vid.split('/')[-1] for vid in video_list]
+    # print(video_list[0:10])
 
-# Generate a pyspark dataframe with video names and video list as columns
-video_df = pd.DataFrame({'video_name': video_names, 'video_path': video_list})
-video_df = spark.createDataFrame(video_df)
+    # Generate a pyspark dataframe with video names and video list as columns
+    video_df = pd.DataFrame({'video_name': video_names, 'video_path': video_list})
+    video_df = spark.createDataFrame(video_df)
 
-# Now need take the video and get the first frame and save that as image in the df
+    # Now need take the video and get the first frame and save that as image in the df
 
-'''
--------------------------------
-Description
--------------------------------
-Function to get the first frame of the video and save it as an image.
+    '''
+    -------------------------------
+    Description
+    -------------------------------
+    Function to get the first frame of the video and save it as an image.
 
--------------------------------
-Parameters
--------------------------------
-video_path: str
-    The path to the video file
+    -------------------------------
+    Parameters
+    -------------------------------
+    video_path: str
+        The path to the video file
 
--------------------------------
-Returns
--------------------------------
-image: np.array
-    The first frame of the video
+    -------------------------------
+    Returns
+    -------------------------------
+    image: np.array
+        The first frame of the video
 
--------------------------------
-'''
-def get_frame(video_path):
-    vidcap = cv2.VideoCapture(video_path)
-    success, image = vidcap.read()
-    if not success:
-        return None
-    save_img = image
-    base64_img = cv2.imencode('.jpg', image)[1]
-    base64_img = base64_img.tobytes()
-    im64 = pybase64.b64encode(base64_img).decode('utf-8')
-    vidcap.release()
-    return im64
+    -------------------------------
+    '''
+    def get_frame(video_path):
+        vidcap = cv2.VideoCapture(video_path)
+        success, first_frame = vidcap.read()
+        if not success:
+            return None, None
+        frame_count = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+        vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_count - 1)
+        success, last_frame = vidcap.read()
+        if not success:
+            return None, None
+        save_img = first_frame
+        base64_img1 = cv2.imencode('.jpg', first_frame)[1]
+        base64_img1 = base64_img1.tobytes()
+        im64_1 = pybase64.b64encode(base64_img1).decode('utf-8')
+        base64_img2 = cv2.imencode('.jpg', last_frame)[1]
+        base64_img2 = base64_img2.tobytes()
+        im64_2 = pybase64.b64encode(base64_img2).decode('utf-8')
+        vidcap.release()
+        return [im64_1, im64_2]
 
-get_frame_udf = udf(get_frame)
+    get_frame_udf = udf(get_frame)
 
-# Get the first frame of the video and save it as an image (this step preferrably in parallel)
-video_df = video_df.withColumn('image', get_frame_udf(video_df['video_path']))
-# print(video_df.head(3))
-final_df = video_df.join(df_csv, video_df.video_name == df_csv.video, 'inner')
-# print(final_df.head(3))
-final_df = final_df.write.csv(save_path + "data.csv", header=True, mode="overwrite")
-# final_df.save.csv('final_df.csv', header=True, mode='overwrite')
+    # Get the first frame of the video and save it as an image (this step preferrably in parallel)
+    
+    video_df = video_df.withColumn('images', get_frame_udf(video_df['video_path']))
+    # video_df = video_df.withColumn('last_frame', get_frame_udf(video_df['video_path'])[1])
+    print(video_df.head(3))
+    final_df = video_df.join(df_csv, video_df.video_name == df_csv.video, 'inner')
+    # print(final_df.head(3))
+    final_df = final_df.write.csv(save_path + "data.csv", header=True, mode="overwrite")
+    # final_df.save.csv('final_df.csv', header=True, mode='overwrite')
 
-spark.stop()
+    spark.stop()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Preprocess the data')
+    parser.add_argument('--csv_file_path', type=str, help='The path to the csv file')
+    parser.add_argument('--video_path', type=str, help='The path to the video files')
+    parser.add_argument('--save_path', type=str, help='The path to save the data')
+    args = parser.parse_args()
+    main(args.csv_file_path, args.video_path, args.save_path)
+    # main('/data/video/video.csv', '/data/video/video/', '/data/video/')
